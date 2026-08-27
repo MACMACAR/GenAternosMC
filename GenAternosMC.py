9152311f5cd1fa4e7c19f874db41bc9d7c558aba
MACMACAR <macmacarofficial@gmail.com>
2026-08-28 02:03:30 +0300

    GenAternosMC.py

--- GenAternosMC.py
+++ GenAternosMC.py
@@ -38,17 +38,22 @@ class LogManager:
         time.sleep(seconds)
 
 # Настройка и инициализация драйвера Chrome с заданными опциями
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
 def setup_chrome_driver():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.headless = True
-    chrome_options.add_argument("start-maximized")
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.binary_location = "/usr/bin/chromium-browser"
+    chrome_options.add_argument("--disable-gpu")
     user_agent_str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
     chrome_options.add_argument(f"--user-agent={user_agent_str}")
-    return uc.Chrome(options=chrome_options)
-
+    
+    service = Service(ChromeDriverManager().install())
+    return webdriver.Chrome(service=service, options=chrome_options)
 # Функция поддержания активности сервера
 def maintain_server_connection(driver):
     print(f'{Fore.LIGHTYELLOW_EX}[ {Fore.LIGHTRED_EX}GenAternosMC {Fore.LIGHTYELLOW_EX}] {Fore.LIGHTBLUE_EX}» Инициализация поддержания активности сервера')
