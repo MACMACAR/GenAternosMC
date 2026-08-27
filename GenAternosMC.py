0ab5b39cc05d3cc3cee8bc5f67e128e3e3196e0a
MACMACAR <macmacarofficial@gmail.com>
2026-08-28 01:55:57 +0300

    GenAternosMC.py

--- GenAternosMC.py
+++ GenAternosMC.py
@@ -42,9 +42,11 @@ def setup_chrome_driver():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.headless = True
     chrome_options.add_argument("start-maximized")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.binary_location = "/usr/bin/google-chrome-stable"
     user_agent_str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
     chrome_options.add_argument(f"--user-agent={user_agent_str}")
-    chrome_options.binary_location = "/usr/bin/google-chrome-stable"
     return uc.Chrome(options=chrome_options)
 
 # Функция поддержания активности сервера
